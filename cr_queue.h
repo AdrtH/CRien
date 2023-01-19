ee25895b1aee261cd885852a001f287f09253761
AdrtH <90867832+AdrtH@users.noreply.github.com>
2023-01-19 22:12:10 +0100

    Changing the type of stack/queue to int (in a macro def)

--- cr_queue.h
+++ cr_queue.h
@@ -4,11 +4,12 @@
 #include <stdlib.h>
 #include <string.h>
 #define CR_MAX_QUEUE_CAP 64000 // SAFE TO CHANGE but that should be enough for everyone
+#define CR_QUEUE_TYPE int
 
 typedef struct queue_t{
-  void*  items[CR_MAX_QUEUE_CAP];
-  size_t front;
-  size_t rear;
+  CR_QUEUE_TYPE items[CR_MAX_QUEUE_CAP];
+  size_t	front;
+  size_t	rear;
 }queue;
 
 size_t length_queue(queue* q)
@@ -18,7 +19,6 @@ size_t length_queue(queue* q)
 
 void shift_queue(queue* q)
 {
-  //  memcpy((void*)q, (void*)(q+sizeof(void*)*(q->front)), length_queue(q)*sizeof(void*));
   size_t length = length_queue(q);
   for(size_t i = 0; i < length; ++i){
     q->items[i] = q->items[i+q->front];
@@ -27,7 +27,7 @@ void shift_queue(queue* q)
   q->front = 0;
 }
 
-void enqueue(queue* q, void* n)
+void enqueue(queue* q, CR_QUEUE_TYPE n)
 {
 
   if(q->rear == CR_MAX_QUEUE_CAP  && q->front ==0){
@@ -42,24 +42,24 @@ void enqueue(queue* q, void* n)
   (q->rear)++;
 }
 
-void* dequeue(queue* q)
+CR_QUEUE_TYPE dequeue(queue* q)
 {
   if(length_queue(q) == 0){
     printf("ERROR: Underflowing queue at address: %p\n",q);
     exit(1);
   }
-  void* ret = q->items[q->front];
+  CR_QUEUE_TYPE ret = q->items[q->front];
   (q->front)++;
   return ret;
 }
 
-void* peek_queue(queue* q)
+CR_QUEUE_TYPE peek_queue(queue* q)
 {
   if(length_queue(q) <= 0){
     printf("ERROR: Underpeeking queue at address: %p\n",q);
     exit(1);
   }
-  void* ret = q->items[q->front];
+  CR_QUEUE_TYPE ret = q->items[q->front];
   return ret;
 }
 
--- cr_stack.h
+++ cr_stack.h
@@ -3,17 +3,18 @@
 
 
 #include <stdlib.h>
-#define CR_MAX_STACK_CAP 64000 //SAFE TO CHANGE but that should be enough for everyone 
+#define CR_MAX_STACK_CAP 64000 //SAFE TO CHANGE but that should be enough for everyone
+#define CR_STACK_TYPE int      //SAFE TO CHANGE but int is good for everyone
 
 
 typedef struct stack_t{
-  void*		items[CR_MAX_STACK_CAP];
+  CR_STACK_TYPE	items[CR_MAX_STACK_CAP];
   size_t	count;
 } cr_stack;
 
 
 // add an element to the stack
-void push(cr_stack *s, void* val)
+void push(cr_stack *s, CR_STACK_TYPE val)
 {
   if(s->count == CR_MAX_STACK_CAP){
     printf("ERROR: Overflowing stack at address: %p\n",s);
@@ -26,19 +27,19 @@ void push(cr_stack *s, void* val)
 // return the element on top then removes it from the stack
 // WARNING: the element is not strictly removes the count is just decremented,
 // so any pointers or for loop with a weird value can access that
-void* pop(cr_stack *s)
+CR_STACK_TYPE pop(cr_stack *s)
 {
   if(s->count == 0){
     printf("ERROR: Underflowing stack at address: %p\n",s);
     exit(1);
   }
   (s->count)--;
-  void* ret = s->items[s->count];
+  CR_STACK_TYPE ret = s->items[s->count];
   return ret;
 }
 
 // work just as if you pop then push the result
-void* peek_stack(cr_stack *s)
+CR_STACK_TYPE peek_stack(cr_stack *s)
 {
   if((s->count) -1 < 0){
     printf("ERROR: Underpeeking stack at address: %p\n",s);
