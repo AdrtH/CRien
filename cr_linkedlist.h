b97e44cc46b3fb45c3441eebb1902c19d24eb410
AdrtH <90867832+AdrtH@users.noreply.github.com>
2023-01-22 00:35:52 +0100

    Ajout de 2 fonction anti-leak dans linkedlist + ajout commentaires

--- cr_linkedlist.h
+++ cr_linkedlist.h
@@ -1,9 +1,15 @@
 #ifndef CR_LINKEDLIST
 #define CR_LINKEDLIST
 
+// Comments in English should be for users of this file,
+// Comments in French are for me, explaining something if I ever need to go back and change something
+// you are free to try and translate french comments, but if you're not planning to do a contribution, you will waste your time, it's not important
+
 #include <stdlib.h>
 #define CR_LINKEDLIST_TYPE int //SAFE TO CHANGE
 
+// WARNING: this can leak memory under certain circumstances,
+// However being careful about removing or freeing your list should prevent any leaking.
 
 typedef struct linkedlist_t {
   CR_LINKEDLIST_TYPE	val;
@@ -16,6 +22,30 @@ linkedlist_t *find_end(linkedlist_t *ll){
     next = next->next;
   }
   return next;
+}
+
+void linkedlist_freeAll(linkedlist_t* ll){
+  // Of course this function is dangerous, all your pointers to anything within the list should not be accessed anymore
+  // If you want to just free a single element, search for the remove function
+  if(ll->next != NULL){
+    linkedlist_freeAll(ll->next);
+  }
+  free(ll);
+}
+
+void linkedlist_remove(linkedlist_t* ll, size_t index)
+{
+  linkedlist_t* next = ll;
+  if(index == 0){
+    free(next);
+    return;
+  }
+  for(int i=0; i < index-1 && next->next != NULL; ++i){ // on va chercher le ultieme element avant l'index à retirer (pour changer son .next essentiellement)
+    next = next->next;
+  }
+  linkedlist_t* tofree = next->next;	    // ce node ne doit plus exister apres remove
+  next->next	       = next->next->next;  // on change l'element suivant par celui d'apres
+  free(tofree);
 }
 
 void linkedlist_append(linkedlist_t* ll, CR_LINKEDLIST_TYPE val)
